196e7936bc44515f93bed0d6a775674efeb7ed3d
matiaspesalaccia <matiaspesalaccia32@gmail.com>
2019-04-02 20:15:09 -0300

    change package.json y server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-const  router = express.Router();
+const router = express.Router();
 const PORT = process.env.PORT || 3000;
 
 
