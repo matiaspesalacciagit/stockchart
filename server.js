f3d7460705acacb478d968a54a43adf159cd44a1
matiaspesalaccia <matiaspesalaccia32@gmail.com>
2019-04-07 14:27:20 -0300

    new server.js

--- server.js
+++ server.js
@@ -1,18 +1,71 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const cors = require('cors'); 
+let express = require('express'),
+cors = require('cors'),
+os = require('os'),
+app = express(),
 const PORT = process.env.PORT || 4200;
-
-app.use(cors());
-
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+ 
+// hard coded configuration object
+conf = {
+  
+    originUndefined: function (req, res, next) {
+ 
+        if (!req.headers.origin) {
+ 
+            res.json({
+ 
+                mess: 'Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined'
+ 
+            });
+ 
+        } else {
+ 
+            next();
+ 
+        }
+ 
+    },
+ 
+    // Cross Origin Resource Sharing Options
+    cors: {
+ 
+        // origin handler
+        origin: function (origin, cb) {
+ 
+            // setup a white list
+            let wl = ['https://api.invertironline.com'];
+ 
+            if (wl.indexOf(origin) != -1) {
+ 
+                cb(null, true);
+ 
+            } else {
+ 
+                cb(new Error('invalid origin: ' + origin), false);
+ 
+            }
+ 
+        },
+ 
+        optionsSuccessStatus: 200
+ 
+    }
+ 
+};
+ 
+// use origin undefined handler, then cors for all paths
+app.use(conf.originUndefined, cors(conf.cors));
+ 
+// get at root
+app.get('/', function (req, res, next) {
+    res.json({
+        mess: 'hello it looks like you are on the whitelist',
+        origin: req.headers.origin,
+        os_hostname: os.hostname(),
+        os_cpus: os.cpus()
+    });
+ 
 });
+ 
 
 app.use(express.static('./dist/stockchart'))
 
