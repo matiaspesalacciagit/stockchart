3c03164f08a5678260d1b9a02d6491baa50947c9
matiaspesalaccia <matiaspesalaccia32@gmail.com>
2019-04-07 14:01:18 -0300

    modif server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var express = require('express');
-// Import the library:
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors'); 
+const PORT = process.env.PORT || 4200;
 
-var app = express();
-
-// Then use it before your routes are set up:
 app.use(cors());
+
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(express.static('./dist/stockchart'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/stockchart/index.html'));
+});
+
+app.listen(PORT, function(){
+    console.log(`Server escuchando en el puerto ${PORT}`);
+});
